ee3108cfcb090c51e0b95596a5b0210a66f58194
Teresa Møller <term@itu.dk>
2022-10-17 16:16:14 +0200

    Defining the views and information

--- ITU_CommunicationApp/app/src/main/java/com/example/itu_communicationapp/ui/home/HomeViewModel.java
+++ ITU_CommunicationApp/app/src/main/java/com/example/itu_communicationapp/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("See RSS newsfeed, suggestions for events, button to write messages");
     }
 
     public LiveData<String> getText() {
--- ITU_CommunicationApp/app/src/main/java/com/example/itu_communicationapp/ui/news/NewsViewModel.java
+++ ITU_CommunicationApp/app/src/main/java/com/example/itu_communicationapp/ui/news/NewsViewModel.java
@@ -10,8 +10,8 @@ public class NewsViewModel extends ViewModel {
 
     public NewsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is news fragment");
-    }
+        mText.setValue("Overview of 'channels' you are subscribed to (e.g. scroll bar, novo nordisk, private group channels') + un/subscribe button, list overview of other channel (new channels first) + un/subscribe button, when clicking on a channel we can see posts related to this channel");
+    } //change name 'news' to 'channels'
 
     public LiveData<String> getText() {
         return mText;
